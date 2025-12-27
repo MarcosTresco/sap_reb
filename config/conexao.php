4071e0dc2192efb55e0468e8274b5891e084c58b
MarcosTresco <jomimasaude@gmail.com>
2025-12-27 09:19:32 -0300

    Update conexao.php

--- config/conexao.php
+++ config/conexao.php
@@ -1,1 +1,13 @@
+<?php
+$host = 'sql300.infinityfree.com';
+$db   = 'if0_39413457_db_sap_reb'; // Nome que combinamos
+$user = 'if0_39413457';       // Ajuste se necessário
+$pass = 'U8ksZNLeIiroL';           // Ajuste se necessário
 
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro na conexão: " . $e->getMessage());
+}
+?>
